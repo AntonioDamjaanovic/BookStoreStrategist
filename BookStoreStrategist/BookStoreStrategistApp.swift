bd778dde63fa4a846c8d821db6c44a675f633b05
AntonioDamjaanovic <damjanovic1109@gmail.com>
2026-03-12 17:13:33 +0100

    Initial commit

---
+++ BookStoreStrategist/BookStoreStrategistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreStrategistApp.swift
+//  BookStoreStrategist
+//
+//  Created by Antonio Damjanović on 12.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct BookStoreStrategistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
