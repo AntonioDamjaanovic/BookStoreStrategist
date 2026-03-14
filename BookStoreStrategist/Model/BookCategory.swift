4f3e76adb335e8de4465cfdf101c90c6e993d19e
AntonioDamjaanovic <damjanovic1109@gmail.com>
2026-03-14 19:40:12 +0100

    Add models

---
+++ BookStoreStrategist/Model/BookCategory.swift
@@ -0,0 +1,36 @@
+//
+//  BookCategory.swift
+//  BookStoreStrategist
+//
+//  Created by Antonio Damjanović on 14.03.2026..
+//
+
+import Foundation
+
+enum BookCategory: Identifiable, CaseIterable {
+    case fiction
+    case biography
+    case children
+    case computerScience
+    case fantasy
+    case business
+    
+    var id: Self { return self }
+    
+    var displayName: String {
+        switch self {
+            case .fiction:
+                "Fiction"
+            case .biography:
+                "Biography"
+            case .children:
+                "Children"
+            case .computerScience:
+                "Computer Science"
+            case .fantasy:
+                "Fantasy"
+            case .business:
+                "Business"
+        }
+    }
+}
